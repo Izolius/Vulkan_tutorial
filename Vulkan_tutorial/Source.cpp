d5328f2c3c41437e74741a4fd46f37f9469d2f71
Izolius <andreev-09@yandex.ru>
2019-10-16 11:42:38 +0500

    resizable window

--- Vulkan_tutorial/Source.cpp
+++ Vulkan_tutorial/Source.cpp
@@ -164,7 +164,7 @@ private:
 	void initWindow() {
 		glfwInit();
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 		glfwSetWindowUserPointer(window, this);
 		glfwSetFramebufferSizeCallback(window,
